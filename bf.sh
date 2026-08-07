43dbb9d1384b8f948b563a92dd91a9a5e6abd8f4
rock3tsprocket <rock-pie@hotmail.com>
2026-08-07 17:12:13 +0100

    IT'S ALIVE

--- bf.sh
+++ bf.sh
@@ -2,9 +2,20 @@
 
 # I apologize for what you're about to see
 
+# Check for argv[1]
+if [ -z $1 ]; then
+    echo "Error: No file was specified"
+    exit 1
+fi
+
+# Check if the file pointed to by argv[1] exists
+if [ ! -f $1 ]; then
+    echo "Error: The specified file does not exist"
+    exit 1
+fi
+
 # Initialize some variables
-code="+++++++++++++[>++++++++<-]>."
-#code="+++++++++++++++++++++++++++++++++."
+code=$(cat $1)
 memory=""
 ip=1
 dp=1 # Data pointer
@@ -12,7 +23,7 @@ dp=1 # Data pointer
 i=1
 while [ $i -le 30000 ]; do
     export memory="${memory}0;"
-    export i=$(($i+1))
+    export i=$((${i}+1))
 done
 
 # Define function for adding/subtracting a thing in the memory
@@ -20,13 +31,9 @@ add() {
     array=$1
     index=$2
     y=$3
-
     
     x=$(echo $array | cut -d ';' -f $index)
-    x=$(( (x+y)%256 ))
-    if [ $x -lt 0 ]; then
-        x=$(( x+256 ))
-    fi
+    x=$(( ${x}+${y} ))
 
     if [ $index -eq 1 ]; then
         array_before=""
@@ -44,22 +51,21 @@ jump_table=""
 sp=1
 i=1
 while [ $i -le ${#code} ]; do
-    export jump_table="${jump_table}1;"
+    export jump_table="${jump_table}0;"
     export i=$(( ${i}+1 ))
 done
 i=1
 while [ $i -le ${#code} ]; do
-    if [ $(echo $code | cut -c $i) = "[" ]; then
+    if [ "$(echo $code | cut -c $i)" = "[" ]; then
         export stack="${stack}/${i}"
         export sp=$(( ${sp}+1 ))
-    elif [ $(echo $code | cut -c $i) = "]" ]; then
+    elif [ "$(echo $code | cut -c $i)" = "]" ]; then
         jump_table=$(add $jump_table $i $(basename ${stack}))
         jump_table=$(add $jump_table $(echo $jump_table | cut -d ';' -f $i) ${i})
         export sp=$(( ${sp}-1 ))
         export stack=$(echo $stack | cut -d '/' -f 1-${sp})
     fi
     export i=$(( ${i}+1 ))
-#    echo $jump_table
 done
 
 # Main loop
@@ -69,9 +75,22 @@ while [ $ip -le ${#code} ]; do
     case $instruction in
         "+")
             memory=$(add $memory $dp 1)
+            data=$(echo $memory | cut -d ';' -f $dp)
+            #echo old add $data
+            if [ $data -gt 255 ]; then
+                memory=$(add $memory $dp -256)
+            fi
+            #echo new add $(echo $memory | cut -d ';' -f $dp)
+            #echo "memory" $(echo $memory | cut -d ';' -f $dp)
             ;;
         "-")
             memory=$(add $memory $dp -1)
+            data=$(echo $memory | cut -d ';' -f $dp)
+            #echo old $data
+            if [ $data -lt 0 ]; then
+                memory=$(add $memory $dp 256)
+            fi
+            #echo new $(echo $memory | cut -d ';' -f $dp)
             ;;
         ">")
             dp=$(( (${dp}+1) % 30000 ))
@@ -85,8 +104,12 @@ while [ $ip -le ${#code} ]; do
         ",")
             echo -n "Input: "
             read -r char
-            char=$(echo $char | cut -c 1)
-            char=$(printf "%d\n" \'${char})
+            if [ -z "$char" ]; then
+                char=0
+            else
+                char=$(echo $char | cut -c 1)
+                char=$(printf "%d\n" \'${char})
+            fi
 
             if [ $dp -eq 1 ]; then
                 memory_before=""
@@ -95,13 +118,15 @@ while [ $ip -le ${#code} ]; do
             fi
             memory_after=$(echo $memory | cut -d ';' -f $(( ${dp}+1 ))- )
             memory="${memory_before}${char};${memory_after}"
+            echo $memory | cut -d ';' -f ${dp}
             ;;
         ".")
             printf "\x$(printf "%x" $(echo $memory | cut -d ';' -f $dp))"
+            #echo $memory | cut -d ';' -f $dp
             ;;
         "[")
             if [ $(echo $memory | cut -d ';' -f $dp) = 0 ]; then
-                ip=$(( $(echo $jump_table | cut -d ';' -f $ip)-1 ))
+                ip=$(echo $jump_table | cut -d ';' -f $ip)
             fi
             ;;
         "]")
