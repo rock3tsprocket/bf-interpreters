607d9aad90a728a97f4affb22a23b1f05a7fc6a5
rock3tsprocket <rock-pie@hotmail.com>
2026-08-07 02:26:31 +0100

    get jump table generation working

---
+++ bf.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Initialize some variables
+code="+++++++++++++[>++++++++<-]>."
+memory=""
+ip=1
+dp=1 # Data pointer
+
+i=1
+while [ $i -le 30000 ]; do
+    export memory="${memory}0;"
+    export i=$(($i+1))
+done
+
+# Define function for adding/subtracting a thing in the memory
+add() {
+    array=$1
+    index=$2
+    y=$3
+
+    
+    x=$(echo $array | cut -d ';' -f $index)
+    x=$(( (x+y)%256 ))
+    if [ $x -lt 0 ]; then
+        x=$(( x+256 ))
+    fi
+
+    if [ $index -eq 1 ]; then
+        array_before=""
+    else
+        array_before="$(echo $array | cut -d ';' -f -$(( ${index}-1 )));"
+    fi
+    array_after=$(echo $array | cut -d ';' -f $(( ${index}+1 ))- )
+    array="${array_before}${x};${array_after}"
+
+    echo $array
+}
+
+stack=""
+jump_table=""
+sp=1
+i=1
+while [ $i -le ${#code} ]; do
+    export jump_table="${jump_table}1;"
+    export i=$(( ${i}+1 ))
+done
+i=1
+while [ $i -le ${#code} ]; do
+    if [ $(echo $code | cut -c $i) = "[" ]; then
+        export stack="${stack}/${i}"
+        export sp=$(( ${sp}+1 ))
+    elif [ $(echo $code | cut -c $i) = "]" ]; then
+        jump_table=$(add $jump_table $i $(basename ${stack}))
+        jump_table=$(add $jump_table $(echo $jump_table | cut -d ';' -f $i) ${i})
+        export sp=$(( ${sp}-1 ))
+        export stack=$(echo $stack | cut -d '/' -f 1-${sp})
+    fi
+    export i=$(( ${i}+1 ))
+#    echo $jump_table
+done
+
+echo $jump_table
